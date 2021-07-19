2e0768ad2c0c75f0412f6bf1cbc57aecd6ce3e84
nortonlui <nortonlui@gmail.com>
2021-07-19 15:59:21 -0300

    First commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+function getDate() {
+  let today = new Date();
+  let dd = String(today.getDate()).padStart(2, '0');
+  let mm = String(today.getMonth() + 1).padStart(2, '0');
+  let yyyy = today.getFullYear();
+  today = dd + '/' + mm + '/' + yyyy;
+  return {
+    today,
+    day: dd,
+    month: mm,
+    year: yyyy,
+  };
+}
+
+document.getElementById('year').textContent = getDate().year;
+
+// Configure Slider
+$('.carousel').carousel({
+  interval: 6000,
+  pause: 'hover',
+});
